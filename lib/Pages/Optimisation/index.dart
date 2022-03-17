a08faed5ab1a7810e72e0eaf5336c99723797175
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-03-17 11:56:21 +0100

    ready to generate pdf

--- lib/Pages/Optimisation/index.dart
+++ lib/Pages/Optimisation/index.dart
@@ -1,14 +1,19 @@
+import 'dart:io';
+
+import 'package:filesystem_picker/filesystem_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mplex/Model/Class/probleme.dart';
+import 'package:mplex/Model/constante.dart';
 import 'package:mplex/Model/enum.dart';
 import 'package:mplex/Pages/Optimisation/op_form_canonique.dart';
 import 'package:mplex/Pages/Optimisation/op_form_standard.dart';
 import 'package:mplex/Pages/Optimisation/op_tableau.dart';
 import 'package:mplex/Widgets/mini.dart';
 import 'package:provider/provider.dart';
-
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'op_fonction_objective.dart';
 
 class Optimisation extends StatefulWidget {
@@ -60,58 +65,93 @@ class _OptimisationState extends State<Optimisation> {
                                 ),
                                 onPressed: () => Get.back()),
                           ),
-                          ValueListenableBuilder(
-                              valueListenable:
-                                  Hive.box("sauvegarde").listenable(),
-                              builder: (context, Box b, _) {
-                                List<Probleme> problemes =
-                                    b.values.toList().cast();
-                                bool exist = problemes.any(
-                                    (element) => element == widget.probleme);
-                                return Tooltip(
-                                  message: exist
-                                      ? "Retirer le problème"
-                                      : "Sauvegarder le problème",
-                                  child: AnimatedContainer(
-                                    duration: const Duration(milliseconds: 500),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                          // color: exist
-                                          //     ? Colors.red
-                                          //     : Get.theme.backgroundColor,
-                                          borderRadius:
-                                              BorderRadius.circular(5)),
-                                      child: ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                              primary: exist
-                                                  ? Colors.red[300]
-                                                  : null),
-                                          child: Icon(
-                                            exist
-                                                ? Icons.bookmark_remove
-                                                : Icons.bookmark_add,
-                                            color: exist
-                                                ? null
-                                                : Get.theme.primaryColor,
-                                          ),
-                                          onPressed: () {
-                                            if (exist) {
-                                              b.deleteAt(b.values
-                                                  .toList()
-                                                  .indexOf(widget.probleme));
-                                              // b.clear();
-                                              // b.addAll(problemes.where(
-                                              //     (element) =>
-                                              //         element !=
-                                              //         widget.probleme));
-                                            } else {
-                                              b.add(widget.probleme);
-                                            }
-                                          }),
+                          ...[
+                            ElevatedButton(
+                                onPressed: () async {
+                                  final pdf = pw.Document();
+
+                                  pdf.addPage(pw.Page(
+                                      pageFormat: PdfPageFormat.a4,
+                                      build: (pw.Context context) {
+                                        return pw.Center(
+                                          child: pw.Text("Hello World"),
+                                        ); // Center
+                                      }));
+                                  String? path = await FilesystemPicker.open(
+                                    title: 'Save to folder',
+                                    context: context,
+                                    rootDirectory: Directory.current,
+                                    fsType: FilesystemType.folder,
+                                    pickText: 'Enregistrer le pdf',
+                                    folderIconColor: Get.theme.primaryColor,
+                                  );
+                                  if (path != null) {
+                                    Constante.log.v(path);
+                                    final file = File(path + "/example.pdf");
+                                    await file.writeAsBytes(await pdf.save());
+                                  }
+                                },
+                                child: Icon(
+                                  Icons.picture_as_pdf,
+                                  color: Colors.black12,
+                                )),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            ValueListenableBuilder(
+                                valueListenable:
+                                    Hive.box("sauvegarde").listenable(),
+                                builder: (context, Box b, _) {
+                                  List<Probleme> problemes =
+                                      b.values.toList().cast();
+                                  bool exist = problemes.any(
+                                      (element) => element == widget.probleme);
+                                  return Tooltip(
+                                    message: exist
+                                        ? "Retirer le problème"
+                                        : "Sauvegarder le problème",
+                                    child: AnimatedContainer(
+                                      duration:
+                                          const Duration(milliseconds: 500),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            // color: exist
+                                            //     ? Colors.red
+                                            //     : Get.theme.backgroundColor,
+                                            borderRadius:
+                                                BorderRadius.circular(5)),
+                                        child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                                primary: exist
+                                                    ? Colors.red[300]
+                                                    : null),
+                                            child: Icon(
+                                              exist
+                                                  ? Icons.bookmark_remove
+                                                  : Icons.bookmark_add,
+                                              color: exist
+                                                  ? null
+                                                  : Get.theme.primaryColor,
+                                            ),
+                                            onPressed: () {
+                                              if (exist) {
+                                                b.deleteAt(b.values
+                                                    .toList()
+                                                    .indexOf(widget.probleme));
+                                                // b.clear();
+                                                // b.addAll(problemes.where(
+                                                //     (element) =>
+                                                //         element !=
+                                                //         widget.probleme));
+                                              } else {
+                                                b.add(widget.probleme);
+                                              }
+                                            }),
+                                      ),
                                     ),
-                                  ),
-                                );
-                              })
+                                  );
+                                })
+                          ]
                         ],
                       ),
                     ),
